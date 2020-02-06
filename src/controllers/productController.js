dd036bed540a795e56ae9272aaa40d0475ad4c80
Ivanlomejor <lolarre7@gmail.com>
2020-02-06 20:40:12 -0300

    Update productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -84,7 +84,7 @@ res.json({
     });
 };
 // Buscar y Actualizar o Guardar
-exports.put = function (req, res) {
+exports.actualizar = function (req, res) {
     const update = req.body;
     const {marca, nombre, codigo} =req.body;
     var filter='';
