b0b653ab0eadd1a8d362fe293b208b052c34851d
Iván Hartmann <lolarre7@gmail.com>
2020-03-24 18:06:56 -0300

    fix error

--- src/controllers/ventaController.js
+++ src/controllers/ventaController.js
@@ -1,7 +1,7 @@
 // Importar el modelo para guardar en la base de datos
-Producto = require('../models/ventaModel');
+Ventas = require('../models/ventaModel');
 exports.new = function (req, res) {
-    var ventaRealizada = new Producto();
+    var ventaRealizada = new Ventas();
     ventaRealizada.fecha= req.body.fecha;
     ventaRealizada.productos= req.body.productos;
     ventaRealizada.total= req.body.total;
@@ -19,7 +19,7 @@ exports.new = function (req, res) {
 }
 
  exports.index = function (req, res) {
-        Producto.get(function (err, ventas) {
+        Ventas.get(function (err, ventas) {
         
             if (err) {
             res.json({
